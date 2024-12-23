ea679f3659e44c77c5ad9554ec786f92be970d3d
大嘴 <jiangbilly@gmail.com>
2024-12-23 10:28:57 +0800

    Initial Commit

---
+++ Murmur/MurmurApp.swift
@@ -0,0 +1,17 @@
+//
+//  MurmurApp.swift
+//  Murmur
+//
+//  Created by Dazui on 2024/12/23.
+//
+
+import SwiftUI
+
+@main
+struct MurmurApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MurmurTests/MurmurTests.swift
@@ -0,0 +1,17 @@
+//
+//  MurmurTests.swift
+//  MurmurTests
+//
+//  Created by Dazui on 2024/12/23.
+//
+
+import Testing
+@testable import Murmur
+
+struct MurmurTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
