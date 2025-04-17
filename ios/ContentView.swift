5364c36b00fed0f60d9adbc377f3490401f9c1ce
大嘴 <jiangbilly@gmail.com>
2025-04-17 14:53:38 +0800

    github dmg

---
+++ ios/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ios
+//
+//  Created by Dazui on 2025/1/3.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ios/iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  iosApp.swift
+//  ios
+//
+//  Created by Dazui on 2025/1/3.
+//
+
+import SwiftUI
+
+@main
+struct iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
